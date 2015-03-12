c55fead2cca2f32a80fd4900a16d5f342b9b9c2a
jrwells <jrwells10@gmail.com>
2015-03-12 16:01:32 -0500

    viewport height css

--- js/script.js
+++ js/script.js
@@ -43,17 +43,6 @@ $(function() {
     });
 });
 
-function hideAddressBar() {
-    if(!window.location.hash) {
-        if(document.height < window.outerHeight) {
-            document.body.style.height = (window.outerHeight + 50) + 'px';
-        }
-        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-    }
-}
-window.addEventListener("load", function() { if(!window.pageYOffset){ hideAddressBar(); } } );
-window.addEventListener("orientationchange", hideAddressBar );
-
 /* Set navigation dots to an active state as the user scrolls */
 function redrawNavigation() {
     $('nav#primary').removeClass('invert');
