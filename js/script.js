7a7c94db4ceffe65aa44d31fae509264f4eb48d6
jrwells <jrwells10@gmail.com>
2015-03-12 19:33:54 -0500

    revert sizes and percent, add resize js function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var introBottom, portfolioBottom, pageBottom;
 
+var bg = jQuery("body");
+jQuery(window).resize("resizeBackground");
+function resizeBackground() {
+    bg.height(jQuery(window).height());
+}
+resizeBackground();
+
 $(function() {
     introBottom =  $('#intro').outerHeight() / 2;
     portfolioBottom =  $('#intro').outerHeight() + ($('#portfolio').outerHeight() / 2);
